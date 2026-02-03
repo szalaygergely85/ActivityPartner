c5aca099b537c4ce525c75f7a3d7b8ce0507246c
Gergely Szalay <gergely.szalay@liferay.com>
2026-02-03 23:31:36 +0100

    remove flyway...

--- src/main/resources/db/migration/V4__Add_birth_date_to_users.sql
+++
@@ -1,8 +0,0 @@
--- Add birth_date column to users table
--- Note: Column already exists from previous migration attempt, so only ensuring constraints
-
--- Update any NULL values
-UPDATE users SET birth_date = '2000-01-01' WHERE birth_date IS NULL;
-
--- Make the column NOT NULL
-ALTER TABLE users MODIFY COLUMN birth_date DATE NOT NULL;
--- src/main/resources/db/migration/V5__Drop_age_column_from_users.sql
+++
@@ -1,3 +0,0 @@
--- Drop redundant age column (age is calculated from birth_date)
--- Note: MySQL doesn't support IF EXISTS for columns, so this will fail if column doesn't exist
-ALTER TABLE users DROP COLUMN age;
